d7c73c51a07473e6bf17b7b64573b4c07a33bd39
Marie Pinto <gozzpi28@gmail.com>
2021-03-20 22:23:45 -0500

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Menu from "./MenuComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutusComponent"
 import DishDetails from "./DishDetailsComponent";
 import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/comments";
@@ -61,11 +62,8 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
-          <Route
-            exact
-            path="/menu"
-            component={() => <Menu dishes={this.state.dishes} />}
-          />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />}/>
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route path="/contactus" component={Contact} />
           <Redirect to="/home" />
